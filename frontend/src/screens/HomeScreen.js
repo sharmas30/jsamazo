9c5b8acdfba05ebc940bc5dd572b7e3d5cd480d0
unknown <shubhsharmass24@gmail.com>
2020-09-18 21:21:56 +0530

    render dynamic produt

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import HomeScreen from './screens/HomeScreen.js'
+const router = () => {
+    const main = document.getElementById('main-container');
+    main.innerHTML = HomeScreen.render();
+};
+window.addEventListener('load', router);
+$ {
+    products.map(
+            (product) => `
+      <li>
+        <div class="product">
+          <a href="/#/product/${product._id}">
+            <img src="${product.image}" alt="${product.name}" />
+          </a>
+        <div class="product-name">
+          <a href="/#/product/1">
+            ${product.name}
+          </a>
+        </div>
+        <div class="product-brand">
+          ${product.brand}
+        </div>
+        <div class="product-price">
+          $${product.price}
+        </div>
+        </div>
+      </li>
+      `
+        )
+        .join('\n')
+}
+`;
+  },
+};
+export default HomeScreen;
