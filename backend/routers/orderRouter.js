c9a469ed1a1f891d58fc6f1160812d0bdc71d02d
unknown <shubhsharmass24@gmail.com>
2020-09-19 22:55:07 +0530

    display order history

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -5,6 +5,11 @@ import Order from '../models/orderModel';
 
 const orderRouter = express.Router();
 
+orderRouter.get('/mine', isAuth, expressAsyncHandler(async(req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+}));
+
 orderRouter.get(
     '/:id',
     isAuth,
