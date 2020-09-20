23cf05a7314aa870e05d316251335af1c915aedd
unknown <shubhsharmass24@gmail.com>
2020-09-20 13:37:06 +0530

    delete product

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,5 +1,6 @@
 import DashboardMenu from '../components/DashboardMenu';
-import { createProduct, getProducts } from '../api';
+import { createProduct, getProducts, deleteProduct } from '../api';
+import { hideLoading, rerender, showLoading, showMessage } from '../utils';
 
 const ProductListScreen = {
         after_render: () => {
@@ -14,6 +15,22 @@ const ProductListScreen = {
             Array.from(editButtons).forEach((editButton) => {
                 editButton.addEventListener('click', () => {
                     document.location.hash = `/product/${editButton.id}/edit`;
+                });
+            });
+
+            const deleteButtons = document.getElementsByClassName('delete-button');
+            Array.from(deleteButtons).forEach((deleteButton) => {
+                deleteButton.addEventListener('click', async() => {
+                    if (confirm('Are you sure to delete this product?')) {
+                        showLoading();
+                        const data = await deleteProduct(deleteButton.id);
+                        if (data.error) {
+                            showMessage(data.error);
+                        } else {
+                            rerender(ProductListScreen);
+                        }
+                        hideLoading();
+                    }
                 });
             });
         },
