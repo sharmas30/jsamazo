df86a95df74d3929b8b0e1d8648480d3feb122ed
unknown <shubhsharmass24@gmail.com>
2020-09-20 13:26:16 +0530

    upload product image & add build scripts _2

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,38 +1,20 @@
-{
-    "name": "jsamazon",
-    "version": "1.0.0",
-    "description": "",
-    "main": "index.js",
-    "scripts": {
-        "start": "nodemon --watch backend --exec babel-node backend/server.js",
-        "build": "rm -rf dest && babel backend -d dist",
-        "serve": "node dist/server.js",
-        "test": "echo \"Error: no test specified\" && exit 1"
+import express from 'express';
+import multer from 'multer';
+import { isAuth, isAdmin } from '../utils';
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, 'uploads/');
     },
-    "repository": {
-        "type": "git",
-        "url": "git+https://github.com/sharmas30/jsamazo.git"
+    filename(req, file, cb) {
+        cb(null, `${Date.now()}.jpg`);
     },
-    "author": "",
-    "license": "ISC",
-    "bugs": {
-        "url": "https://github.com/sharmas30/jsamazo/issues"
-    },
-    "homepage": "https://github.com/sharmas30/jsamazo#readme",
-    "dependencies": {
-        "body-parser": "^1.19.0",
-        "cors": "^2.8.5",
-        "dotenv": "^8.2.0",
-        "express": "^4.17.1",
-        "express-async-handler": "^1.1.4",
-        "jsonwebtoken": "^8.5.1",
-        "mongoose": "^5.10.6",
-        "multer": "^1.4.2"
-    },
-    "devDependencies": {
-        "@babel/cli": "^7.11.6",
-        "@babel/core": "^7.11.6",
-        "@babel/node": "^7.10.5",
-        "@babel/preset-env": "^7.11.5"
-    }
-}
+});
+
+const upload = multer({ storage });
+const uploadRouter = express.Router();
+
+uploadRouter.post('/', isAuth, isAdmin, upload.single('image'), (req, res) => {
+    res.status(201).send({ image: `/${req.file.path}` });
+});
+export default uploadRouter;
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,44 +1,43 @@
-{
-    "name": "jsamazon",
-    "version": "1.0.0",
-    "description": "",
-    "main": "index.js",
-    "scripts": {
-        "start": "nodemon --watch backend --exec babel-node backend/server.js",
-        "build": "rm -rf dest && babel backend -d dist",
-        "serve": "node dist/server.js",
-        "test": "echo \"Error: no test specified\" && exit 1"
-    },
-    "repository": {
-        "type": "git",
-        "url": "git+https://github.com/sharmas30/jsamazo.git"
-    },
-    "author": "",
-    "license": "ISC",
-    "bugs": {
-        "url": "https://github.com/sharmas30/jsamazo/issues"
-    },
-    "homepage": "https://github.com/sharmas30/jsamazo#readme",
-    "dependencies": {
-        "body-parser": "^1.19.0",
-        "cors": "^2.8.5",
-        "dotenv": "^8.2.0",
-        "express": "^4.17.1",
-        "express-async-handler": "^1.1.4",
-        "jsonwebtoken": "^8.5.1",
-        "mongoose": "^5.10.6",
-        "multer": "^1.4.2"
-    },
-    "devDependencies": {
-        "@babel/cli": "^7.11.6",
-        "@babel/core": "^7.11.6",
-        "@babel/node": "^7.10.5",
-        "@babel/preset-env": "^7.11.5"
+import axios from 'axios';
+import { apiUrl } from './config';
+import { getUserInfo } from './localStorage';
+
+export const getProducts = async() => {
+    try {
+        const response = await axios({
+            url: `${apiUrl}/api/products`,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.statusText !== 'OK') {
+            throw new Error(response.data.message);
+        }
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return { error: err.response.data.message || err.message };
     }
-}
-console.log(err);
-return { error: err.response.data.message || err.message };
-}
+};
+
+export const getProduct = async(id) => {
+    try {
+        const response = await axios({
+            url: `${apiUrl}/api/products/${id}`,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.statusText !== 'OK') {
+            throw new Error(response.data.message);
+        }
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        return { error: err.response.data.message || err.message };
+    }
 };
 
 export const createProduct = async() => {
